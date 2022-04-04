3eb50046b6538cf66fc3a367bd02b2add6e686c3
tatadavila <tatdev91@gmail.com>
2022-04-04 10:43:48 -0500

    Add feature on click task the status change

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/task.css";
 
-const Task = ({ item }) => {
+const Task = ({ item, onClickItem }) => {
   return (
-    <div className="card">
+    <div
+      className={`card ${item.complete ? "done" : ""}`}
+      onClick={() => onClickItem(item.id - 1)}
+    >
       <div>{item.task}</div>
+      <div className={`status ${item.complete ? "done" : ""}`}>
+        ({item.complete ? "Done" : "Pending"})
+      </div>
     </div>
   );
 };
